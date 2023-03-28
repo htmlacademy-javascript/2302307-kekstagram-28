4e150a0c83f93c8c96ed9cca374d3f4191593680
anvts <worontsov.andrey@gmail.com>
2023-03-28 04:03:23 +0300

    Implements content filters

--- js/main.js
+++ js/main.js
@@ -1,36 +1,21 @@
 import { drawPhotos } from './thumbnails.js';
-import { openModal, closeModal } from './modal.js';
-import { insertData } from './modal-data.js';
 import { showErrorAlert } from './alert.js';
 import { getData } from './api.js';
-import { Route } from './constants.js';
+import { RENDERER_DELAY, Route } from './constants.js';
 import { ErrorText } from './constants.js';
+import { showFiltersSection, setClick, reRenderThumbnails } from './content-filters.js';
+import { debounce } from './util.js';
 
 import './user-form.js';
 
 try {
   const photosData = await getData(Route.GET_DATA);
   drawPhotos(photosData);
-
-  const pictureNodes = document.querySelectorAll('.pictures a');
-  const modalCloseBtnNode = document.querySelector('#picture-cancel');
-
-  pictureNodes.forEach((pictureNode) => {
-    pictureNode.addEventListener('click', (evt) => {
-      openModal();
-      insertData(evt, photosData);
-    });
-  });
-
-  modalCloseBtnNode.addEventListener('click', () => {
-    closeModal();
-  });
+  showFiltersSection();
+  setClick(debounce((btnNode) => {
+    reRenderThumbnails(photosData, btnNode);
+  }, RENDERER_DELAY));
 
 } catch {
   showErrorAlert(ErrorText.GET_DATA);
 }
-
-
-/**
- * СДЕЛАТЬ ПУНКТ 2 ИЗ ОТПРАВКИ ДАННЫХ. СДЕЛАТЬ ПОКАЗ СООБЩЕНИЯ ДЛЯ УСПЕШНОЙ ОТПРАВКИ ДАННЫХ
- */
